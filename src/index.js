22121ccf5863ec2826798d233c8dc879954b7254
Fares El-Shazly <fares.elshazlyy@gmail.com>
2021-05-25 21:25:11 +0200

    Add Speech Recognition Configurations

--- src/index.js
+++ src/index.js
@@ -22,6 +22,15 @@ const runPythonCode = (req, res, next) => {
       console.log(typeof message);
     }
   );
+  PythonShell.run(
+    "/home/fares/Desktop/GP_ML/Speech Recognition/prediction.py",
+    null,
+    function (err, message) {
+      if (err) console.log(err);
+      console.log(message);
+      console.log(typeof message);
+    }
+  );
   console.log("out");
   next();
 };
@@ -36,30 +45,56 @@ socket.on("connect", function (connection) {
 
   connection.on("message", function (message) {
     console.log("Received Message: " + message.utf8Data);
+    const mouse = robotjs.getMousePos();
 
     if (message.utf8Data === "1") {
-      // robotjs.mouseClick("left");
+      robotjs.mouseClick("left");
       console.log("mouse left clicked");
     } else if (message.utf8Data === "2") {
-      // robotjs.mouseClick("right");
+      robotjs.mouseClick("right");
       console.log("mouse right clicked");
     } else if (message.utf8Data === "3") {
-      // robotjs.mouseClick("left", true);
+      robotjs.mouseClick("left", true);
       console.log("mouse double left clicked");
     } else if (message.utf8Data === "4") {
       robotjs.mouseClick("right", true);
       console.log("mouse double right clicked");
+
+      // * Speech Recognition Configurations
+    } else if (message.utf8Data === "Left") {
+      robotjs.moveMouseSmooth(mouse.x - 50, mouse.y);
+      console.log("Move Mouse Left");
+    } else if (message.utf8Data === "Up") {
+      robotjs.moveMouseSmooth(mouse.x, mouse.y - 50);
+      console.log("Move Mouse Up");
+    } else if (message.utf8Data === "Right") {
+      robotjs.moveMouseSmooth(mouse.x + 50, mouse.y);
+      console.log("Move Mouse Right");
+    } else if (message.utf8Data === "Down") {
+      robotjs.moveMouseSmooth(mouse.x, mouse.y + 50);
+      console.log("Move Mouse Down");
+    } else if (message.utf8Data === "Yes") {
+      robotjs.mouseClick("left");
+      console.log("Mouse Left Click");
+    } else if (message.utf8Data === "No") {
+      robotjs.mouseClick("right");
+      console.log("Move Right Click");
+
     } else {
       const { posX, posY } = robotjs.getMousePos();
       const coordinates = message.utf8Data.toString().split(" ");
 
       if (
-        Math.abs(posX - coordinates[0] < 10) &&
-        Math.abs(posY - coordinates[1] < 10)
+        Math.abs(posX - coordinates[0] < 20) &&
+        Math.abs(posY - coordinates[1] < 20)
       )
         return;
-
-      robotjs.moveMouse(screen.width - coordinates[0], coordinates[1]);
+      robotjs.moveMouseSmooth(
+        screen.width - coordinates[0],
+        coordinates[1],
+        0.7
+      );
+      // robotjs.moveMouse();
       socket.broadcast(message.utf8Data);
     }
 
